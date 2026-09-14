a3f4bca56abe122c6406c9d911696cd01965bbb1
Cocotrilo <nogalesisaac25@gmail.com>
2026-09-14 14:19:34 -0500

    Added tweens to simulate speed

--- player.gd
+++ player.gd
@@ -5,18 +5,29 @@ const GRAVITY = 1200.0
 var gravity_direction = 1
 const FLIP_IMPULSE = 700.0
 
+
+
 func _on_flip():
 	var tween = create_tween()
 	tween.tween_property($Sprite2D, "scale", Vector2(0.7, 1.3), 0.05)
 	tween.tween_property($Sprite2D, "scale", Vector2(1,1), 0.1)
 
+func _on_land():
+	var tween = create_tween()
+	tween.tween_property($Sprite2D, "scale", Vector2(1.3, 0.7), 0.05)
+	tween.tween_property($Sprite2D, "scale", Vector2(1,1), 0.1)
 func _physics_process(delta):
+	
+	var was_in_air = not (is_on_floor() or is_on_ceiling())
+	
 	velocity.y += GRAVITY * gravity_direction * delta
 	velocity.x = SPEED
 	
 	
 	
 	if is_on_floor() or is_on_ceiling():
+		if was_in_air:
+			_on_land()
 		if Input.is_action_just_pressed("move_up"):
 			_on_flip()
 			gravity_direction = -1
